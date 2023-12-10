9de88109860c12b117b35d26cb9bdc5fda1e49a1
Buck Brady <bbrady145@gmail.com>
2023-12-09 23:19:34 -0700

    feat: add War, Wallet, and Universe Topics

---
+++ topics.go
@@ -0,0 +1,37 @@
+package queue
+
+const (
+	// Crons
+	TOPIC_CRONJOB_STATUS = "cronjob.status"
+
+	// Universe Endpoint
+	TOPIC_CRONJOB_UNIVERSE_ANCESTRY     = "cronjob.universe.ancestry"
+	TOPIC_CRONJOB_UNIVERSE_BLOODLINE    = "cronjob.universe.bloodline"
+	TOPIC_CRONJOB_UNIVERSE_CATEGORY     = "cronjob.universe.category"
+	TOPIC_CRONJOB_UNIVERSE_FACTION      = "cronjob.universe.faction"
+	TOPIC_CRONTJOB_UNIVERSE_RACE        = "cronjob.universe.race"
+	TOPIC_CRONJOB_UNIVERSE_SYSTEM_JUMPS = "cronjob.universe.system_jumps"
+	TOPIC_CRONJOB_UNIVERSE_SYSTEM_KILLS = "cronjob.universe.system_kills"
+
+	// Jobs
+	TOPIC_JOB_WARS_KILLMAILS                  = "job.wars.killmails"
+	TOPIC_JOB_WALLET_CHARACTER                = "job.wallet.character.balance"
+	TOPIC_JOB_WALLET_CHARACTER_JOURNAL        = "job.wallet.character.journal"
+	TOPIC_JOB_WALLET_CHARACTER_TRANSACTIONS   = "job.wallet.character.transactions"
+	TOPIC_JOB_WALLET_CORPORATION              = "job.wallet.corporation.balance"
+	TOPIC_JOB_WALLET_CORPORATION_JOURNAL      = "job.wallet.corporation.journal"
+	TOPIC_JOB_WALLET_CORPORATION_TRANSACTIONS = "job.wallet.corporation.transactions"
+	TOPIC_JOB_UNIVERSE_ASTEROID_BELT          = "job.universe.asteroid_belt"
+	TOPIC_JOB_UNIVERSE_CATEGORY               = "job.universe.category"
+	TOPIC_JOB_UNIVERSE_CONSTELLATION          = "job.universe.constellation"
+	TOPIC_JOB_UNIVERSE_GROUP                  = "job.universe.group"
+	TOPIC_JOB_UNIVERSE_MOON                   = "job.universe.moon"
+	TOPIC_JOB_UNIVERSE_PLANET                 = "job.universe.planet"
+	TOPIC_JOB_UNIVERSE_REGION                 = "job.universe.region"
+	TOPIC_JOB_UNIVERSE_SYSTEM                 = "job.universe.system"
+	TOPIC_JOB_UNIVERSE_STARGATE               = "job.universe.stargate"
+	TOPIC_JOB_UNIVERSE_STAR                   = "job.universe.star"
+	TOPIC_JOB_UNIVERSE_STRUCTURES             = "job.universe.structures"
+	TOPIC_JOB_UNIVERSE_STATIONS               = "job.universe.stations"
+	TOPIC_JOB_UNIVERSE_TYPE                   = "job.universe.type"
+)
